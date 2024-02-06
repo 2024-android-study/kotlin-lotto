922fb83ed944f8ff1e971d3bccc6faaa86358f45
권민채 <minchae075@naver.com>
2024-02-06 22:27:09 +0900

    feat: 구매 금액, 당첨 번호, 보너스 번호 입력 추가 및 검증

---
+++ src/main/kotlin/lotto/constants/ViewConstant.kt
@@ -0,0 +1,10 @@
+package lotto.constants
+
+object ViewConstant {
+    const val INPUT_MONEY = "구입금액을 입력해 주세요."
+    const val INPUT_LOTTO_NUM = "당첨 번호를 입력해 주세요."
+    const val INPUT_BONUS_NUM = "보너스 번호를 입력해 주세요."
+
+    const val OUTPUT_NUM_OF_PURCHASE = "%d개를 구매했습니다."
+    const val LOTTO_STATISTICS = "당첨 통계\n---"
+}
---
+++ src/main/kotlin/lotto/validation/InputValidator.kt
@@ -0,0 +1,59 @@
+package lotto.validation
+
+import lotto.constants.ErrorConstant
+import lotto.constants.NumConstant
+
+class InputValidator {
+
+    // 구입 금액 검증
+    fun validateMoney(input: String): Int {
+        require(input.isNotEmpty()) {ErrorConstant.ERROR_EMPTY}
+        require(input.contains(" ")) {ErrorConstant.ERROR_MONEY_BLANK}
+        require(isNumber(input)) {ErrorConstant.ERROR_NUM}
+        require(input.toInt() < 0) {ErrorConstant.ERROR_MONEY_MINUS}
+        require(input.toInt() < 1000) {ErrorConstant.ERROR_MONEY_MIN}
+        require(input.toInt() % 1000 > 0) {ErrorConstant.ERROR_MONEY_ZERO_UNIT}
+
+        return input.toInt()
+    }
+
+    // 당첨 번호 검증
+    fun validateLottoNum(input: String): List<Int> {
+        require(input.isNotEmpty()) {ErrorConstant.ERROR_EMPTY}
+
+        val numbers = input.split(",")
+
+        require(numbers.size == NumConstant.LOTTO_CNT) {ErrorConstant.ERROR_NUM_CNT}
+        require(numbers.toSet().size == NumConstant.LOTTO_CNT) {ErrorConstant.ERROR_NUM_DUPLICATE}
+
+        numbers.forEach {
+            require(isNumber(it)) {ErrorConstant.ERROR_NUM}
+            require(it.toInt() in (NumConstant.MIN_RANDOM_NUM..NumConstant.MAX_RANDOM_NUM)) {ErrorConstant.ERROR_NUM_RANGE}
+        }
+
+        return numbers.map { it.toInt() }
+    }
+
+    // 보너스 번호 검증
+    fun validateBonusNum(input: String, lottoNumbers: MutableList<Int>): Int {
+        require(input.isNotEmpty()) {ErrorConstant.ERROR_EMPTY}
+        require(input.contains(" ")) {ErrorConstant.ERROR_MONEY_BLANK}
+        require(input.contains(",")) {ErrorConstant.ERROR_BONUS_NUM}
+        require(isNumber(input)) {ErrorConstant.ERROR_NUM}
+        require(input.toInt() in (NumConstant.MIN_RANDOM_NUM..NumConstant.MAX_RANDOM_NUM)) {ErrorConstant.ERROR_NUM_RANGE}
+        require(isDuplicateBonusNum(input.toInt(), lottoNumbers)) {ErrorConstant.ERROR_BONUS_NUM_DUPLICATE}
+
+        return input.toInt()
+    }
+
+    // 보너스 번호가 당첨 번호와 중복되는지 확인
+    private fun isDuplicateBonusNum(num: Int, lottoNumbers: MutableList<Int>): Boolean {
+        lottoNumbers.add(num)
+        return lottoNumbers.toSet().size == NumConstant.LOTTO_BONUS_CNT
+    }
+
+    // 숫자인지 확인
+    private fun isNumber(input: String): Boolean {
+        return input.all { ch -> ch.isDigit() }
+    }
+}
