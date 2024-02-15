17881ece5205b1f5168bcb9c29b0ca7ee9192905
Nahyun Kim <hedghog1004@naver.com>
2024-02-16 03:33:39 +0900

    feat: 상수 목록 작성

---
+++ src/main/kotlin/lotto/constant/NumConstant.kt
@@ -0,0 +1,15 @@
+package lotto.constant
+
+object NumConstant {
+    const val NUMBER_RANGE_START = 1
+    const val NUMBER_RANGE_END = 45
+
+    const val LOTTO_NUM = 6
+    const val LOTTO_PRICE = 1000
+
+    const val WIN_3 = 5000
+    const val WIN_4 = 50000
+    const val WIN_5 = 1500000
+    const val WIN_5_AND_BONUS = 30000000
+    const val WIN_6 = 2000000000
+}
