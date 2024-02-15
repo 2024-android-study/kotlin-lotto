904871e498cc36347ed8835309482bb5ee292cd7
Nahyun Kim <hedghog1004@naver.com>
2024-02-16 05:12:28 +0900

    feat: 당첨 번호 유효성을 체크하는 validator 메서드 구현
    - 개수가 6개인지
    - 각각 숫자로 이루어져 있는지
    - 각각 숫자의 범위가 1~45가 맞는지
    - 중복되지 않은 숫자로만 이루어져 있는지

--- src/main/kotlin/lotto/constant/ErrorMessageConstant.kt
+++ src/main/kotlin/lotto/constant/ErrorMessageConstant.kt
@@ -3,4 +3,8 @@ package lotto.constant
 object ErrorMessageConstant {
     const val ERROR_NOT_NUM = "[ERROR] 유효한 숫자가 입력되어야 합니다."
     const val ERROR_NOT_DIVIDED = "[ERROR] 구입 금액은 ${NumConstant.LOTTO_PRICE}로 나누어 떨어져야 합니다."
+
+    const val ERROR_LOTTO_NUM_SIZE = "[ERROR] 로또 번호는 ${NumConstant.LOTTO_NUM}개여야 합니다."
+    const val ERROR_LOTTO_NUM_RANGE = "[ERROR] 로또 번호는 ${NumConstant.NUMBER_RANGE_START}부터 ${NumConstant.NUMBER_RANGE_END} 사이의 숫자여야 합니다."
+    const val ERROR_LOTTO_DUPLICATION = "[ERROR] 로또 번호는 중복되지 않은 숫자여야 합니다."
 }
